be0c0249e90edc2c2e51df787318be3d1e95d216
Karlyweatherstein <karlyweatherstein@gmail.com>
2022-06-27 10:34:43 -0400

    adding service worker and idb connections

---
+++ public/js/idb.js
@@ -0,0 +1,4 @@
+// variable to hold db connection
+let db;
+// establish a connection to IndexedDB database called 'pizza_hunt' and set it to version 1
+const request = indexedDB.open("BudgetTracker", 1);
