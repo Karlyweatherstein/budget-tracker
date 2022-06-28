802dbccdb43ff6ce517cad6a195c41a5d57b8c09
Karlyweatherstein <karlyweatherstein@gmail.com>
2022-06-27 21:52:18 -0400

    deploy

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
@@ -6,7 +7,7 @@ const compression = require("compression");
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost/budgetTracker";
-
+console.log(MONGODB_URI);
 const app = express();
 
 app.use(logger("dev"));
@@ -20,6 +21,14 @@ app.use(express.static("public"));
 mongoose.connect(MONGODB_URI || "mongodb://localhost:27017/budgetTracker", {
   useNewUrlParser: true,
   useFindAndModify: false,
+});
+mongoose.connection.on("connected", function () {
+  console.log("Mongoose default connection open");
+});
+
+// If the connection throws an error
+mongoose.connection.on("error", function (err) {
+  console.log("Mongoose default connection error: " + err);
 });
 
 // routes
